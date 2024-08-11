d4cf6188dba1df51c88fd76d277523eb522c4c20
helmyyfly <helmyzx6@gmail.com>
2024-08-12 00:45:18 +0700

    added + modified

--- main.js
+++ main.js
@@ -18,7 +18,6 @@ window.addEventListener("mousemove", function (e) {
 //    }, (duration: 500, fill: "forwards"));
    
 });
-// end cursor
 // scroll  sections active link
 
 const sections = document.querySelectorAll("section[id]");
@@ -27,15 +26,14 @@ function scrollActive() {
     const scrollY = window.pageXOffset;
 
     sections.forEach((current) => {
-        const sectionHeight = current.
-        offsetHeight,
+        const sectionHeight = current.offsetHeight,
         sectionTop = current.offsetTop - 50,
         sectionId = current.getAttribute("id");
 
         if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
         document.querySelector(".nav__menu a [href*=" + sectionId + "]").classList.add("active-link");
         } else {
-        document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.remove('active-link');
+        document.querySelector(".nav__menu a[href*=" + sectionId + "]").classList.remove("active-link");
         }
     });
 }
@@ -50,7 +48,7 @@ function scrollHeader() {
 // light dark theme 
 const themeButton = document.getElementById("theme-button");
 const lightTheme = "light-theme";
-const iconTheme = "bx-sun"
+const iconTheme = "bx-sun";
 
 const selectedTheme = localStorage.getItem("selected-theme");
 const selectedIcon = localStorage.getItem("selected-icon");
