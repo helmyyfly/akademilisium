f21a68c313a28d9f46c76bfac629fc7e80a24c75
helmyyfly <helmyzx6@gmail.com>
2024-08-15 22:55:04 +0700

    modified

--- main.js
+++ main.js
@@ -34,32 +34,26 @@ function scrollActive()  {
 window.addEventListener("scroll", scrollActive);
 
 // light mode and dark mode 
-function scrollHeader() {
-    const header = document.getElementById("header");
-    if (this.scrollY >= 80) header.classList.add("scroll-header");
-    else header.classList.remove ("scroll-header");
-}
 
-// light dark theme
-const themeButton =  document.getElementById("theme-button");
+const themeButton = document.getElementById ("theme-button");
 const lightTheme = "light-theme";
 const iconTheme = "bx-sun";
 
 const selectedTheme = localStorage.getItem("selected-theme");
-const selectedIcon = localStorage.getItem("selected-icon");
+const selectedICon = localStorage.getItem("selected-icon");
 
-const getCurrentTheme = () => (document.body.classList.contains(lightTheme) ? "dark" : "light");
-const getCurrentIcon = () => (document.body.classList.contains(iconTheme) ? "bx  bx-moon" : "bx bx-sun");
+const getCurrentTheme = () => (document.body.classList.contains(lightTheme) ? "dark" : "light"); 
+const getCurrentIcon = () => (document.body.classList.contains(iconTheme) ? "bx bx-moonn" : "bx bx-sun"); 
 
-if(selectedTheme) {
-    document.addEventListener[selectedTheme === "dark" ? "add" : "remove"] (lightTheme);
-    themeButton.classList[selectedIcon === "bx bx-moon" ? "add" : "remove"] (iconTheme);
+if (selectedTheme) {
+    document.addEventListener[selectedTheme === "dark" ? "add" : "remove"](lightTheme);
+    themeButton.classList[selectedICon === "bx bx-moon" ? "add" : "remove"](iconTheme);
 }
-// active and non-active dark mode 
+
 themeButton.addEventListener("click", () => {
-    document.body.classList.toggle (lightTheme);
+    document.body.classList.toggle(lightTheme);
     themeButton.classList.toggle(iconTheme);
 
     localStorage.setItem("selected-theme", getCurrentTheme());
     localStorage.setItem("selected-icon", getCurrentIcon());
-});
+})
